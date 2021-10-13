1839a3ed85b528e47d799df5e761356039f58bd5
Maksym <maksym.vechirko@axiomc.com>
2021-10-13 10:40:54 +0300

    Initial Commit

---
+++ UControl/UControl.h
@@ -0,0 +1,18 @@
+//
+//  UControl.h
+//  UControl
+//
+//  Created by Maksym on 13/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UControl.
+FOUNDATION_EXPORT double UControlVersionNumber;
+
+//! Project version string for UControl.
+FOUNDATION_EXPORT const unsigned char UControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UControl/PublicHeader.h>
+
+
